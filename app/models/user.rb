1832afa4a9faa401a297614cf41ea3f1dc07e715
rkrstl <69182634+rkrstl@users.noreply.github.com>
2020-08-25 13:06:29 +0900

    ”ユーザー情報をDBに保存する”を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        #  VALID_PASSWORD_REGEX =/\A[a-z0-9]+\z/i
+        #  validates :password, presence: true,length:{minimum:6}, format: { with: VALID_PASSWORD_REGEX }
+        #  validates :nickname, presence: true, lendth:{maximum: 6 }
+        #  validates :email,presence:true,uniqueness: true, confirmation: true
+
+  
 end
+
