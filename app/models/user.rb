9cc0479b6b6e9ec4ed9ce20b3e982f3f2cf6ce7a
rkrstl <69182634+rkrstl@users.noreply.github.com>
2020-08-26 14:58:26 +0900

    validationの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,43 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+        #  パスワードが必須であること
+        #  パスワードは6文字以上であること
+        #  パスワードは半角英数字混合であること
+        #  パスワードは確認用を含めて2回入力すること
+         VALID_PASSWORD_REGEX =/\A[a-zA-Z0-9]+\z/
          validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
-         validates :nickname, presence: true, length:{maximum: 6 }
 
+        #  ニックネームが必須であること
+         validates :nickname, presence: true
+
+        #  ユーザー本名が、名字と名前がそれぞれ必須であること
+        #  ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させること
          validates :first_name,presence:true,format:{with:/\A[ぁ-んァ-ン一-龥]/}
          validates :last_name,presence:true,format: {with:/\A[ぁ-んァ-ン一-龥]/}
+
+        #  ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること
+        #  ユーザー本名のフリガナは全角（カタカナ）で入力させること
          validates :first_name_kana,presence:true,format:{with:/\A[ァ-ヶー－]+\z/}
          validates :last_name_kana,presence:true,format:{with:/\A[ァ-ヶー－]+\z/}
+
+        #  生年月日が必須であること
          validates :birth,presence:true
-         validates :email,presence:true,uniqueness: true
+
+
+        #  メールアドレスが必須であること
+        #  メールアドレスが一意性であること
+        # @が含まれていること
+         validates :email,presence:true,uniqueness: true,format:{with: /\A\S+@\S+\.\S+\z/}
   
 end
+
+
+
+
+
+
+
+
+
 
