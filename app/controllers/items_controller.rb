6cbea09ca4c91486881ae7f7fc1e930b8d073ad5
rkrstl <69182634+rkrstl@users.noreply.github.com>
2020-09-01 14:16:15 +0900

    部分テンプレートで編集画面と出品画面をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,15 @@ before_action :move_to_index,except:[:index,:show]
         @item=Item.find(params[:id])
       end
 
+      def edit
+        @item=Item.find(params[:id])
+      end
+
+      def update
+        item=Item.find(params[:id])
+        item.update(item_params)
+      end
+
   
 
   private
