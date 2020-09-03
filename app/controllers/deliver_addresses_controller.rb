9fe1cd28427d3b8ec2445be83b3b86b94fcffa71
rkrstl <choiihwa68@gmail.com>
2020-09-03 14:40:09 +0900

    出品者本人は直接URLを入力するとトップ画面に遷移

--- app/controllers/deliver_addresses_controller.rb
+++ app/controllers/deliver_addresses_controller.rb
@@ -1,5 +1,6 @@
 class DeliverAddressesController < ApplicationController
   before_action :move_to_login,only:[:index]
+  before_action :move_to_indexx,only:[:index]
 
   def index
     @item = Item.find(params[:item_id])
@@ -11,6 +12,11 @@ class DeliverAddressesController < ApplicationController
        redirect_to new_user_session_path
      end
    end
+
+  def move_to_indexx
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+   end
+  end
  end
-#  modelのアソシエーション変更
-# ordersのビューをディばりーに変更
