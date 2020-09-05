ee67be69a81089155e2b16e9634ae6c7b8770ef4
rkrstl <choiihwa68@gmail.com>
2020-09-05 15:14:38 +0900

    単体テストコード

--- app/controllers/deliver_addresses_controller.rb
+++ app/controllers/deliver_addresses_controller.rb
@@ -4,9 +4,27 @@ class DeliverAddressesController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order = UserOrder.new
+  end
+
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = UserOrder.new(delivery_params)
+    if @order.valid?
+      pay_item
+      @order.save  # バリデーションをクリアした時
+     redirect_to root_path
+    else
+      render "index"    # バリデーションに弾かれた時
+    end
   end
 
    private
+   def delivery_params
+     params.permit(:token,:prefecture_id,:postal_code,:city,:address1,:telephone).merge(user_id: current_user.id,item_id:params[:item_id])
+   end
+
    def move_to_login
      unless user_signed_in?
        redirect_to new_user_session_path
@@ -18,5 +36,15 @@ class DeliverAddressesController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
    end
+  end
+
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: delivery_params[:token],
+      currency:'jpy'
+    )
   end
  end
---
+++ spec/factories/user_orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user_order do
+    prefecture_id {"1"}
+    postal_code   {"111-1111"}
+    city          {"あわ"}
+    address1      {"あわ"}
+    telephone     {"11111111111"}
+
+  end
+end
